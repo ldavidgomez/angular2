f8fe1df05f0486032aa025f6296402417c187f44
ldavidgomez <ldavid.github@gmail.com>
2016-09-01 13:38:48 +0200

    first commit

---
+++ angular2-quickstar/app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by david on 1/09/16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+platformBrowserDynamic().bootstrapModule(AppModule);
