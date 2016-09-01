8b45387fbc16015e354068b1ad56d212054b8f8e
ldavidgomez <ldavid.github@gmail.com>
2016-09-01 17:13:22 +0200

    added hero-detail-component, refactor hero class to its own file

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,7 +1,9 @@
 /**
  * Created by david on 31/08/16.
  */
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Hero } from './hero.ts'
 
 // Mocking data that should be retrieved from a data service
 const HEROES: Hero[] = [
@@ -17,62 +19,6 @@ const HEROES: Hero[] = [
     {id: 20, name: 'Tornado'}
 ];
 
-styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`]
-
-
-export class Hero {
-    id: number;
-    name: string;
-}
-
 @Component({
     selector: 'my-app',
     template: `
@@ -85,14 +31,7 @@ export class Hero {
             <span class="badge">{{hero.id}}</span> {{hero.name}}
           </li>
         </ul>
-        <div *ngIf="selectedHero">
-          <h2>{{selectedHero.name}} details!</h2>
-          <div><label>id: </label>{{selectedHero.id}}</div>
-          <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="First name">
-          </div>
-      </div>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
       
   `,
 
---
+++ angular2-tour-of-heroes/app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by david on 1/09/16.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
