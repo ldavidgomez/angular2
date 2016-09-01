05abe944fb1d553d45a4d4d7d6a631eb10f70c5c
ldavidgomez <ldavid.github@gmail.com>
2016-09-01 17:50:24 +0200

    added service for mock-heroes

---
+++ angular2-tour-of-heroes/app/hero.service.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by david on 1/09/16.
+ */
+import { Injectable } from '@angular/core';
+
+import {Hero} from "./hero";
+import {HEROES} from "./mock-heroes";
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Hero {
+     return HEROES;
+    }
+}
