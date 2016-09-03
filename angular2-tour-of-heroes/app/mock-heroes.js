b7af3a664863fdbb9ef5a1dd997fabb21db9f679
ldavidgomez <ldavid.github@gmail.com>
2016-09-03 12:29:54 +0200

    commented mock-hero

--- angular2-tour-of-heroes/app/mock-heroes.js
+++ angular2-tour-of-heroes/app/mock-heroes.js
@@ -1,15 +1,20 @@
-"use strict";
-// Mocking data that should be retrieved from a data service
-exports.HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+/**
+ * Created by david on 1/09/16.
+ */
+////Replaced by in-memory-data.service
+// import {Hero} from "./hero";
+//
+// // Mocking data that should be retrieved from a data service
+// export const HEROES: Hero[] = [
+//     {id: 11, name: 'Mr. Nice'},
+//     {id: 12, name: 'Narco'},
+//     {id: 13, name: 'Bombasto'},
+//     {id: 14, name: 'Celeritas'},
+//     {id: 15, name: 'Magneta'},
+//     {id: 16, name: 'RubberMan'},
+//     {id: 17, name: 'Dynama'},
+//     {id: 18, name: 'Dr IQ'},
+//     {id: 19, name: 'Magma'},
+//     {id: 20, name: 'Tornado'}
+// ]; 
 //# sourceMappingURL=mock-heroes.js.map
