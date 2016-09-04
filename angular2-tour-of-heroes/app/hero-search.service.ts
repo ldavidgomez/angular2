ea7e7110e2c40fc303acc9647467368857b85715
ldavidgomez <ldavid.github@gmail.com>
2016-09-04 11:34:33 +0200

    fixed catch error

--- angular2-tour-of-heroes/app/hero-search.service.ts
+++ angular2-tour-of-heroes/app/hero-search.service.ts
@@ -3,6 +3,7 @@
  */
 import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
+
 import { Observable } from 'rxjs';
 
 import { Hero }           from './hero';
@@ -10,6 +11,7 @@ import { Hero }           from './hero';
 @Injectable()
 export class HeroSearchService {
     constructor(private http: Http) {}
+
     search(term: string): Observable<Hero[]> {
         return this.http
             .get(`app/heroes/?name=${term}`)
